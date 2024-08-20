080f78460527b21e0283e0168349c50b4e593bf7
Ishan Verma <ishanv002@gmail.com>
2024-08-21 01:25:27 +0530

    Understanding virtual dom, hooks and Jsx in a better manner and moving forward to tailwind

--- 01basicreact/01vitereact/src/App.jsx
+++ 01basicreact/01vitereact/src/App.jsx
@@ -1,14 +1,15 @@
 import Chai from "./Chai.jsx"
 
 function App() {
+  const userName = "Reacting to React"
 
   return (
     <>
       <Chai/>
-      <h1>Reacting to Chai</h1>
+      <h1>Reacting to Chai {userName}</h1>
       <p>testing my chai</p>
     </>
   )
 }
-
+ 
 export default App
