80faf049cf6e57dc575f0b926cb2d1b7dd18949d
Ishan Verma <ishanv002@gmail.com>
2024-08-21 22:41:54 +0530

    interview question and 1st project start

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Ishan",
+    age: 20
+  }
+  let newArr = [1, 3, 5]
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card userName="Understanding React" btnText="click Me" />
+      <Card userName="Ishan" />
     </>
   )
 }
---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className="w-full h-screen"></div>
+  )
+}
+
+export default App
